fe2f58fe85b6fd8a368be32de6fadda0519a5cbb
Carlosmarquez27 <carlos27marquez10@gmailcom>
2024-07-05 23:11:37 -0500

    Actualizado info de funciones

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 export const PORT = process.env.PORT || 3000;
-export const User = process.env.User || "carlos.marquez";
-export const Password = process.env.Password || "*Cens.2828"
+export const User = process.env.User;
+export const Password = process.env.Password;
